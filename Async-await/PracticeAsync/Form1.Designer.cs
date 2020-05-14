47f4ebaa0378483cf7bbfe0a27de38154d2b33b2
Oleh Harmash <oleh_harmash@epam.com>
2020-05-14 20:16:07 +0300

    add Win Form

---
+++ Async-await/PracticeAsync/Form1.Designer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace PracticeAsync
+{
+    partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var request = WebRequest.Create(textBox1.Text);
+            var content = new MemoryStream();
+
+            using (var responce = request.GetResponse())
+            {
+                using ( var responceStream = responce.GetResponseStream())
+                {
+                    responceStream.CopyTo(content);
+                }
+            }
+
+            label1.Text = content.Length.ToString();
+        }
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
+
---
+++ Async-await/PracticeAsync/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeAsync
+{
+    public partial class Form1 : Form
+    {
+        
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(424, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1079, 384);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+    }
+}
